06585b703ed772c7e79db35a1c0f5216a45857b8
sherlock07 <yvarshney0707@gmail.com>
2017-08-31 20:06:10 +0530

    Heroku Deployement

---
+++ GenericApp.py
@@ -0,0 +1,116 @@
+from flask import Flask, request
+from pymessenger import Bot
+from GenericUtils import fetch_reply
+import requests,json
+
+app = Flask(__name__)
+
+
+FB_ACCESS_TOKEN = "EAAcGMs2tZCYEBAMurCLKX4gxMZCqp3EAfX9kaNrlSWWW60Y8QnIorbrNPPIds0vFlQBrYLHN51LyJpQHYaKM9oqSCa46GMF4b8Jkb5euIrejglq3P7JguN9DqJrJvwbP0Fr4zwgYwqm5FPJKzgNGriqojeZAiVxWUslyyLLQAZDZD"
+bot = Bot(FB_ACCESS_TOKEN)
+#Webhook verification.
+
+@app.route('/', methods=['GET'])
+def verify():
+	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+		if not request.args.get("hub.verify_token") == "hello":
+			return "Verification token mismatch", 403
+		return request.args["hub.challenge"], 200
+	return "Hello world", 200
+
+#Webhook Verification
+@app.route('/', methods=['POST'])
+def webhook():
+	print(request.data)
+
+	data = request.get_json()
+
+	if data['object'] == "page":
+		entries = data['entry']
+
+		for entry in entries:
+			messaging = entry['messaging']
+
+			for messaging_event in messaging:
+
+				sender_id = messaging_event['sender']['id']
+				recipient_id = messaging_event['recipient']['id']
+
+				if messaging_event.get('message'):
+
+					if messaging_event['message'].get('text'):
+
+						query = messaging_event['message']['text']
+						reply = fetch_reply(query, sender_id)
+
+						if reply['type'] == 'news':
+							bot.send_generic_message(sender_id, reply['data'])
+						else:
+							bot.send_text_message(sender_id, reply['data'])					
+
+				elif messaging_event.get('postback'):
+					if messaging_event['postback']['payload'] ==  'SHOW_HELP':
+						bot.send_text_message(sender_id, "Ok. I will show you help message!")
+	return "ok", 200
+
+
+
+def set_greeting_text():
+    headers = {
+        'Content-Type':'application/json'
+        }
+    data = {
+        "setting_type":"greeting",
+        "greeting":{
+            "text":"Hi {{user_first_name}}! Iam news bot"
+            }
+        }
+    ENDPOINT = "https://graph.facebook.com/v2.8/me/thread_settings?access_token=%s"%(FB_ACCESS_TOKEN)
+    r = requests.post(ENDPOINT, headers = headers, data = json.dumps(data))
+    print(r.content)
+
+
+def set_persistent_menu():
+    headers = {
+        'Content-Type':'application/json'
+        }
+    data = {
+        "setting_type":"call_to_actions",
+        "thread_state" : "existing_thread",
+        "call_to_actions":[
+            {
+                "type":"web_url",
+                "title":"View Website",
+                "url":"http://codingblocks.com/" 
+            },
+            {
+                "type":"web_url",
+                "title":"Enroll now!",
+                "url":"http://students.codingblocks.xyz/purchasable_course"
+            },
+            {
+                "type":"web_url",
+                "title":"Register",
+                "url":"http://codingblocks.com/signup/"
+            }]
+        }
+    ENDPOINT = "https://graph.facebook.com/v2.6/me/thread_settings?access_token=%s"%(FB_ACCESS_TOKEN)
+    r = requests.post(ENDPOINT, headers = headers, data = json.dumps(data))
+    print(r.content)
+
+
+
+
+if __name__ == "__main__":
+	set_persistent_menu()
+	set_greeting_text()
+	app.run(port=8000, use_reloader = True)
+
+"""
+buttons = [{"type":"web_url","url":"https://www.facebook.com/Mycodebot-1293905183961696/","title":"Visit our page"}]
+						bot.send_button_message(sender_id,PAGE_LINK,buttons)
+buttons = [{"type":"postback",
+									"payload": "SHOW_HELP",
+									"title":"Click here for help!"}]
+						bot.send_button_message(sender_id, "Check out this website!",buttons)
+"""
---
+++ GenericUtils.py
@@ -0,0 +1,108 @@
+import apiai
+import json
+import requests
+from utils2 import fetch_repl
+
+from pymongo import MongoClient
+
+
+MONGODB_URI = "mongodb://test:test@ds111754.mlab.com:11754/chatbot"
+client = MongoClient(MONGODB_URI, connectTimeoutMS=30000, socketTimeoutMS=None, socketKeepAlive=True)
+db = client.get_default_database()
+news_records= db.news_records
+# users = db._users    for getting the users.
+
+def getRECORDS(user_id):
+    records = news_records.find({"sender_id":user_id})
+    return records
+
+def pushRECORD(record):
+    news_records.insert_one(record)
+
+record = {"sender_id":"1234",	
+			"news_type":"sports",
+			"language":"hindi",
+			"geo-country":"india"
+		}
+pushRECORD(record)
+
+
+APIAI_ACCESS_TOKEN = "2542abe010a74c7fa9378e7886b66089"
+
+ai = apiai.ApiAI(APIAI_ACCESS_TOKEN)
+
+
+GNEWS_API_ENDPOINT = "https://gnewsapi.herokuapp.com"
+
+
+def get_news(params):
+	params['news'] = params['news_type']
+	resp = requests.get(GNEWS_API_ENDPOINT, params = params)
+	return resp.json()
+
+def apiai_response(query, session_id):
+	"""
+	function to fetch api.ai response
+	"""
+	request = ai.text_request()
+	request.lang='en'
+	request.session_id=session_id
+	request.query = query
+	response = request.getresponse()
+	return json.loads(response.read().decode('utf8'))
+
+
+def parse_response(response):
+	"""
+	function to parse response and 
+	return intent and its parameters
+	"""
+	result = response['result']
+	params = result.get('parameters')
+	intent = result['metadata'].get('intentName')
+	return intent, params
+
+	
+def fetch_reply(query, session_id):
+	"""
+	main function to fetch reply for chatbot and 
+	return a reply dict with reply 'type' and 'data'
+	"""
+	response = apiai_response(query, session_id)
+	intent, params = parse_response(response)
+	#print( "Intent = "+ intent+ "\n")
+	reply = {}
+
+	if intent == None:
+		print ("\n\nIn None\n\n")
+		reply = fetch_repl(query,session_id)
+
+	elif intent == "news":
+		print ("\n\nIn News\n\n")
+		reply['type'] ='news'
+		print(params)
+		params['sender_id'] = session_id
+		pushRECORD(params)
+		articles = get_news(params)
+		news_elements = []
+
+		for article in articles:
+			element = {}
+			element['title'] = article['title']
+			element['item_url'] = article['link']
+			element['image_url'] = article['img']
+			element['buttons'] = [{
+				"type":"web_url",
+				"title":"Read more",
+				"url":article['link']}]
+			news_elements.append(element)
+
+		reply['data'] = news_elements
+
+	elif intent.startswith('smalltalk'):
+		print ("\n\nIn SmallTalk\n\n")
+
+		reply['type'] = 'smalltalk'
+		reply['data'] = response['result']['fulfillment']['speech']
+
+	return reply
